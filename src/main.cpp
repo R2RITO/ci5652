f6494578ace01a23b6b5a101728503085a6fa641
Arturo <arturo.voltattorni@gmail.com>
2015-05-03 18:57:47 -0930

    Comienzo de sol inicial

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,21 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 using namespace std;
 
 typedef double KMcoord; //coordenada
+typedef double dist;    // Distancia entre dos puntos
+typedef vector< vector<dist> > matrizDist; // Matriz de distancia entre puntos.
 typedef vector<KMcoord> KMpoint; // punto
 typedef vector<KMpoint> KMpointArray; // arreglo de puntos
 
 #define KM_SUM(x,y)		((x) + (y))
 #define KM_POW(v)		((v)*(v))
+#define K 5
 
 /* 
  * Calcula la distancia euclideana de 2 puntos
@@ -66,19 +72,57 @@ int main(int argc, char** argv) {
     dim = dataPoints[0].size();
     
     cout << "Numero de datos: " << N << endl << "Dimensiones: " << dim << endl;
-    
+
+    /*
     for(int i=0; i < dataPoints.size(); i++){
         KMpoint point = dataPoints[i];
         for(int j=0; j < point.size(); j++)
             cout << point[j] << ", " ;
         cout << endl;
                    
+    }*/
+
+    cout << "Distancia punto 98 y punto 99 -> " << kmDist(dim,
+            dataPoints[98],
+            dataPoints[99]) << endl;
+
+    matrizDist matriz;
+
+    // Llenar la matriz de distancias.
+    for (int i=0; i < N-1; i++) {
+        vector<dist> columna;        
+        for (int j=i+1; j < N; j++) {
+            columna.push_back(kmDist(dim,dataPoints[i],dataPoints[j]));
+        }
+        matriz.push_back(columna);
     }
-    cout << "Distancia punto 1 y punto 2 -> " << kmDist(dim,
-            dataPoints[1],
-            dataPoints[2]);
     
+    cout << "Distancia punto 98 y punto 99 -> " << matriz[98][0] << endl;
+
+    // Vector de soluciones, posición en el arreglo.
+    vector<int> sols;
+
+    // Generar solucion inicial utilizando metodo k-means++
+    srand(time(NULL));
+
+    sols.push_back(rand() % N);
+
+    for (int i=0; i < K-1; i++) {
+        cout << "";
+    }
     
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
 
